1e69070f1ca53ec9fdec9772f438cfb20c79b6d2
el_gran_matthews <matibaghdassarian@est.frba.utn.edu.ar>
2019-08-13 20:48:34 -0300

    backend modificado

--- src/Backend.elm
+++ src/Backend.elm
@@ -1,8 +1,9 @@
 port module Backend exposing (..)
 import Types exposing(Song)
-
+import List exposing (range, map, intersperse, filter,concat )
 import Utils exposing (..)
 import Models exposing (Model)
+import String exposing(contains)
 
 -- Existe la funcion findSong que recibe
 -- una condicion y una lista de canciones
@@ -20,41 +21,69 @@ import Models exposing (Model)
 
 -- Debería darnos la url de la cancion en base al id
 urlById : String -> List Song -> String
-urlById id songs = ""
+urlById identificacion songs = ( findById identificacion songs ).url
+
+findById : String -> List Song -> Song
+findById identificacion songs = findSong ( matchById identificacion ) songs
+
+matchById : String -> Song -> Bool
+matchById identificacion unaCancion = (identificacion == unaCancion.id)
 
 -- Debería darnos las canciones que tengan ese texto en nombre o artista
 filterByName : String -> List Song -> List Song
-filterByName text songs = songs
+filterByName text songs = filter (matchByText text) songs
+
+matchByText : String -> Song -> Bool
+matchByText texto cancion = matchByName texto cancion.name || matchByName texto cancion.artist
+
+matchByName : String -> String -> Bool
+matchByName texto nombre = contains texto nombre
 
 -- Recibe un id y tiene que likear/dislikear una cancion
 -- switchear song.liked
 toggleLike : String -> List Song -> List Song
-toggleLike id songs = songs
+toggleLike identification songs = map (changeSongStatus identification) songs
+
+changeSongStatus : String -> Song -> Song
+changeSongStatus identification song = if matchById identification song then changeStatus song else song
+
+changeStatus : Song -> Song
+changeStatus song = if song.liked then dislike song else like song
+
+dislike : Song -> Song
+dislike song = {song |liked = False}
+
+like : Song -> Song
+like song = {song |liked = True}
+
 
 -- Esta funcion tiene que decir si una cancion tiene
 -- nuestro like o no, por ahora funciona mal...
 -- hay que arreglarla
 isLiked : Song  -> Bool
-isLiked song = False
+isLiked song = song.liked
 
 -- Recibe una lista de canciones y nos quedamos solo con las que
 -- tienen un like
 filterLiked : List Song -> List Song
-filterLiked songs = songs
+filterLiked songs = filter isLiked songs
 
 -- Agrega una cancion a la cola de reproduccion
 -- (NO es necesario preocuparse porque este una sola vez)
 addSongToQueue : Song -> List Song -> List Song
-addSongToQueue song queue = queue
+addSongToQueue song queue = queue ++ [song]
 
 -- Saca una cancion de la cola
 -- (NO es necesario que se elimine una sola vez si esta repetida)
 removeSongFromQueue : String -> List Song -> List Song
-removeSongFromQueue id queue = queue
+removeSongFromQueue id queue = filter (nonMatchById id) queue
+
+nonMatchById : String -> Song -> Bool
+nonMatchById identificacion unaCancion = (identificacion /= unaCancion.id)
 
 -- Hace que se reproduzca la canción que sigue y la saca de la cola
 playNextFromQueue : Model -> Model
-playNextFromQueue model = model
+playNextFromQueue model = {model | queue = tailSafe model.queue}
 
 -------- Funciones Listas --------
 
